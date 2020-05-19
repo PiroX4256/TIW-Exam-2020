4d8a4b5a82172ea83dee4772c505414069fd33af
Luca Pirovano <luca7.pirovano@mail.polimi.it>
2020-05-19 16:23:27 +0200

    feat: final first project version

--- src/main/java/it/polimi/tiw/controllers/GoToAlbumPage.java
+++ src/main/java/it/polimi/tiw/controllers/GoToAlbumPage.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/GoToAlbumPage")
@@ -28,17 +29,35 @@ public class GoToAlbumPage extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         int albumId = Integer.parseInt(request.getParameter("album"));
+        int pageId = Integer.parseInt(request.getParameter("page"));
         ImageDAO imageDAO = new ImageDAO(connection, albumId);
         List<Image> images;
+        //TODO fix forward/backward images slide
         try {
             images = imageDAO.retrieveImages();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Unable to retrieve album images");
             return;
         }
+        if(((pageId+1)*5)<images.size() && request.getParameter("dir")!=null && request.getParameter("dir").equals("next")) pageId++;
+        else if(pageId>0 && request.getParameter("dir")!=null && request.getParameter("dir").equals("prev")) pageId--;
+        int imageStart = pageId * 5;
+        int imageEnd = imageStart + 4;
+        List<Image> imagesTemp;
+        imagesTemp = new ArrayList<>();
+        for (int i = imageStart; i <= imageEnd; i++) {
+            if (images.size() > i) {
+                imagesTemp.add(images.get(i));
+            }
+        }
+        redirect(request, response, pageId, images, imagesTemp);
+    }
+
+    public void redirect(HttpServletRequest request, HttpServletResponse response, int pageId, List<Image>images, List<Image>imagesTemp) throws IOException {
         WebContext webContext = new WebContext(request, response, getServletContext(), request.getLocale());
-        webContext.setVariable("imageList", images);
+        webContext.setVariable("pageId", pageId);
+        webContext.setVariable("imageList", imagesTemp);
+        webContext.setVariable("imageFull", images);
         String path = Pages.albumPage;
         templateEngine.process(path, webContext, response.getWriter());
     }
